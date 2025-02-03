ecd2fed5a899f916d15eba717f5cb676bc78cfe8
Karthik-Padavala <karthik.padavala77@gmail.com>
2025-02-03 09:24:50 +0530

    Added Dead letter queue for error handling in pdf_gen

--- pdf_generation/main.py
+++ pdf_generation/main.py
@@ -1,8 +1,12 @@
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, KafkaProducer
 import json
 import time
 from generate_pdf import generate_pdf
 
+def custom_serializer(obj):
+    if isinstance(obj, Decimal):
+        return float(obj)
+    return json.dumps(obj)
 
 def consume_messages():
     
@@ -10,18 +14,30 @@ def consume_messages():
         "pdf_gen",
         bootstrap_servers=['localhost:9092'],
         auto_offset_reset='earliest', 
-        enable_auto_commit=True,
+        enable_auto_commit=False,
         group_id='your_consumer_group'
     )
 
+    producer = KafkaProducer(
+        bootstrap_servers=['localhost:9092'],
+        value_serializer=lambda x: json.dumps(x, default=custom_serializer).encode('utf-8'))
+
+
     try:
         for message in consumer:
-            message_data = json.loads(message.value.decode('utf-8'))
-            print("hello!")
-            print(f"Received Message:{message_data}")
-            generate_pdf(message_data)
-            time.sleep(1)
-    except KeyboardInterrupt:
+            try:
+                message_data = json.loads(message.value.decode('utf-8'))
+                print(f"Received Message:{message_data}")
+                generate_pdf(message_data)
+
+                ## commiting kafka message after pdf genertion
+                consumer.commit()
+            except Exception as e:
+                print(f"Error while generating the pdf for user{message_data['name']}")
+                print("Error message:"e)
+
+                producer.send("dlq", message)
+            except KeyboardInterrupt:
         pass
     finally:
         consumer.close()
